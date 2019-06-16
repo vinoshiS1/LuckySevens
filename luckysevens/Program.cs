46025b60d61828a36f806fefe62c41f49ae4a248
Vinoshi <vinoshisilva@yahoo.ca>
2019-06-16 01:12:01 -0600

    Finished Project

---
+++ luckysevens/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace luckysevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1,7);
+                die2= rng.Next(1,7);
+
+                if (die1 + die2 == 7);
+                {
+                    totalNumberOfSevens++;
+                }
+            }
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalNumberOfSevens);
+            Console.ReadLine();
+        }
+    }
+}
